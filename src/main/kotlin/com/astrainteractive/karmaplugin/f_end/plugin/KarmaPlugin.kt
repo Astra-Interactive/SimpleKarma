161af4e163db1111665f4ee65726edfbd333074f
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-04 16:54:30 +0300

    Plugin's companion object was layed

--- src/main/kotlin/com/astrainteractive/karmaplugin/f_end/plugin/KarmaPlugin.kt
+++ src/main/kotlin/com/astrainteractive/karmaplugin/f_end/plugin/KarmaPlugin.kt
@@ -1,6 +1,23 @@
 package com.astrainteractive.karmaplugin.f_end.plugin
 
+import com.astrainteractive.astratemplate.AstraTemplate
+import com.astrainteractive.astratemplate.sqldatabase.Database
+import com.astrainteractive.karmaplugin.f_end.utils.Config
+import com.astrainteractive.karmaplugin.f_end.utils.Files
+import com.astrainteractive.karmaplugin.f_end.utils.Translation
 import org.bukkit.plugin.java.JavaPlugin
 
 class KarmaPlugin : JavaPlugin() {
+    companion object {
+        lateinit var instance: AstraTemplate
+            private set
+        lateinit var translations: Translation
+            private set
+        lateinit var empireFiles: Files
+            private set
+        lateinit var pluginConfig: Config
+            private set
+        public lateinit var database: Database
+            private set
+    }
 }
