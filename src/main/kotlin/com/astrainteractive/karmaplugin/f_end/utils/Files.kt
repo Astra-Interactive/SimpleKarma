45a8d13b6c6d92f2c3d673631ddc03e2692b200c
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-04 16:27:59 +0300

    config file load was added

---
+++ src/main/kotlin/com/astrainteractive/karmaplugin/f_end/utils/Files.kt
@@ -0,0 +1,8 @@
+package com.astrainteractive.karmaplugin.f_end.utils
+
+import com.astrainteractive.astralibs.FileManager
+
+public class Files() {
+    val configFile: FileManager =
+        FileManager("config.yml")
+}
