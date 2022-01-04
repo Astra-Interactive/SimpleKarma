43460d5ae9697ea558a4cd345b6fb8a9cd765f00
Арзуманов Михаил <marzumanov@softrust.ru>
2022-01-04 16:27:04 +0300

    Translations were added

--- src/main/kotlin/com/astrainteractive/karmaplugin/f_end/utils/Translation.kt
+++ src/main/kotlin/com/astrainteractive/karmaplugin/f_end/utils/Translation.kt
@@ -1,4 +1,13 @@
 package com.astrainteractive.karmaplugin.f_end.utils
 
+import com.astrainteractive.astralibs.FileManager
+import com.astrainteractive.astralibs.getHEXString
+
 class Translation {
+    val _translationFile: FileManager = FileManager("translations.yml")
+    private val translation = _translationFile.getConfig()!!
+
+    public val SUCCESS_COMMAND = translation.getHEXString("SUCCESS_COMMAND")?:"Команда выполнена успешно"
+    public val FAILED_COMMAND = translation.getHEXString("FAILED_COMMAND")?:"Команда не была выполнена"
+
 }
